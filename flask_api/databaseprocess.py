b9e9a78f603aeb3441d3f13ee98dba1f4274a288
RamuSannanagari <38727862+RamuSannanagari@users.noreply.github.com>
2020-07-05 12:02:52 +0530

    Update databaseprocess.py

--- flask_api/databaseprocess.py
+++ flask_api/databaseprocess.py
@@ -5,7 +5,7 @@ import pyodbc # connect to SQL server
 import mysql.connector # connect to mysql
 import psycopg2 # connect to postgrasql
 import cx_Oracle # connect to Oracle 
-
+import json
 from utility.logger import log
 
 from.save_connection import Connections
@@ -18,7 +18,7 @@ class Databaseprocess():
 
         if "database" in db_details:
             self.database = db_details["database"]
-        if "Host" in db_details:
+        if "host" in db_details:
             self.host = db_details["host"]
         if "username" in db_details:
             self.username = db_details["username"]
@@ -26,13 +26,13 @@ class Databaseprocess():
             self.password = db_details["password"]
         if "port" in db_details:
             self.port = db_details["port"]
-        if "Drivers" in db_details:
-            self.Drivers = db_details["Drivers"]
+        if "Driver" in db_details:
+            self.Driver = db_details["Driver"]
 
-        if "connectiin_name" in db_details:
-            self.connection_name = db_details["connectiin_name"]
+        if "connection_name" in db_details:
+            self.connection_name = db_details["connection_name"]
 
-    def authenticats(self):
+    def authenticate(self):
         if self.database == 'sqlite':
             connection = sqlite3.connect(self.host)
             status = "successful"
@@ -41,25 +41,32 @@ class Databaseprocess():
             return status
 #Mysql connection
         elif self.database == 'mysql':
-            connection = mysql.connector.connect(self.host)
+            connection = mysql.connector.connect(host=self.host, user=self.username, password=self.password)
             status = "successful"
             if not connection:
                 status = "failed"
             return status
 # postgrasql connection
         elif self.database == 'postgrassql':
-            connection = psycopg2.connect(self.host)
+            connection = psycopg2.connect(user=self.username, host=self.host, password=self.password)
             status = "successful"
             if not connection:
                 status = "failed"
             return status
 # SQL server connection
         elif self.database == 'SQL server':
-            connection = pyodbc.connect(self.host)
+            log.debug("inside the sql")
+            log.debug(self.host+self.username+self.password+self.Driver)
+            #print(self.host, self.username, self.password,self.Driver)
+            connection = pyodbc.connect(self.host, self.username, self.password,self.Driver )
             status = "successful"
-            if not connection:
+
+            if not connection:            
                 status = "failed"
+            connection.close()
+            log.debug(status)
             return status
+
 # Oracle connection
         elif self.database == 'oracle':
             connection = cx_Oracle.connect(self.host)
@@ -67,7 +74,6 @@ class Databaseprocess():
             if not connection:
                 status="failed"
             return status
-        
 
     def save_db_details(self):
         db_obj = Connections()
@@ -89,7 +95,41 @@ class Databaseprocess():
         return data
 
     def get_db_object_metadata(self):
-        pass
+        if self.database =='sqlite':
+            try:
+                connection = sqlite3.connect(self.host)
+                cursor=connection.cursor()
+                select_sql="""
+                       SELECT * FROM sqlite_master
+                        WHERE type='table' 
+                        ORDER BY name;
+                       """
+                cursor.execute(select_sql)
+                data=self.result=cursor.fetchall()
+                json.loads(data)
+            except:
+                self.operation_status=1
+            finally:
+                cursor.close()
+        elif self.database =='sqlserver':
+            try:
+                connection = pyodbc.connect(self.host)
+                cursor= connection.cursor()
+                select_sql="""
+                            select  
+                            TABLE_CATALOG as [Database],
+                            [TABLE_SCHEMA],TABLE_NAME,
+                            count(COLUMN_NAME)as column_count from INFORMATION_SCHEMA.COLUMNS
+                            Group by TABLE_CATALOG,[TABLE_SCHEMA],TABLE_NAME 
+                            order by TABLE_NAME;"""
+            
+                data=self.result=cursor.fetchall()
+                cursor.execute(select_sql)
+                json.loads(data)
+            except:
+                self.operation_status=1
+            finally:
+                cursor.close()    
 
     def get_db_object_data(self):
         pass
